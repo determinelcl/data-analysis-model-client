04cd5bb52c8ac1d89e42da365719b4d8cdc04986
determinelcl <timecyc@outlook.com>
2020-03-13 11:54:11 +0800

    添加View

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <template>
-    <div id="app">
-      <Login></Login>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If ViewUI is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
+        below
+      </p>
+      <Button type="primary">Button</Button>
     </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Login from "./components/Login";
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'App',
-        components: {
-            Login,
-        }
-    }
-
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    #nav {
-        padding: 30px;
-    }
-
-    #nav a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
